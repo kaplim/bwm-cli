f4afdd94f76c210bc274b4670649c7a9e2b53a82
Kap Lim <freekaplim@gmail.com>
2018-10-30 12:48:28 -0400

    Add register, login and auth middleware routes on server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config/dev');
 const FakeDb = require('./fake-db');
 
 const rentalRoutes = require('./routes/rentals');
+const userRoutes = require('./routes/users');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
 	const fakeDb = new FakeDb();
-	fakeDb.seedDb();
+	//fakeDb.seedDb();
 });
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/v1/rentals', rentalRoutes);
+app.use('/api/v1/users', userRoutes);
 
 const PORT = process.env.PORT || 3001;
 
